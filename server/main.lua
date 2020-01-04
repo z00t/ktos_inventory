c8bc94b1fdb4c44f3c6bec98c2b56e028c94f745
Ktoś <43894510+Ktos93@users.noreply.github.com>
2020-01-04 19:30:53 +0100

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,15 @@
 RegisterServerEvent("player:getItems")
-RegisterServerEvent('player:savInvSv')
 RegisterServerEvent("item:giveItem")
 local invTable = {}
+
+data = {}
+local inventory = {}
+data = inventory
+
+AddEventHandler('redem_inventory:getData', function(cb)
+    cb(data)
+end)
+
 AddEventHandler("player:getItems", function()
     local _source = source
 
@@ -19,8 +27,7 @@ AddEventHandler("player:getItems", function()
                     for i,k in pairs(invTable) do
                         if k.id == identifier and k.charid == charid then
                             TriggerClientEvent("gui:getItems", _source, k.inventory)
-                            break
-                        end
+                            break end
                     end
 
                 else
@@ -38,8 +45,7 @@ AddEventHandler("player:getItems", function()
                     for i,k in pairs(invTable) do
                         if k.id == identifier and k.charid == charid then
                             TriggerClientEvent("gui:getItems", _source, k.inventory)
-                            break
-                        end
+                            break end
                     end
                 end
         end)
@@ -74,65 +80,65 @@ AddEventHandler('player:savInvSv', function(source, id)
                     end
                 end)
 
-                break
-            end
+                break end
         end
 
     end)
 end)
 
 AddEventHandler("item:add", function(source, arg, identifier , charid)
-        local _source = source
-        for i,k in pairs(invTable) do
-            if k.id == identifier and k.charid == charid then
+    local _source = source
+    for i,k in pairs(invTable) do
+        if k.id == identifier and k.charid == charid then
 
-                local name = tostring(arg[1])
-                local amount = arg[2]
-                if k.inventory[(name)] ~= nil then
-                   local val = k.inventory[name]
-                   newVal = val + amount
-                   print(val)
-                   print(qty)
-                   print(newVal)
-                   k.inventory[name]= tonumber(math.floor(newVal))
-                   TriggerClientEvent("gui:getItems", _source, k.inventory)
-               else
-                   TriggerEvent("item:new",_source, name, amount, identifier , charid)
-                end
+            local name = tostring(arg[1])
+            local amount = arg[2]
+            if k.inventory[(name)] ~= nil then
+                local val = k.inventory[name]
+                newVal = val + amount
+                print(val)
+                print(qty)
+                print(newVal)
+                k.inventory[name]= tonumber(math.floor(newVal))
+                TriggerClientEvent("gui:getItems", _source, k.inventory)
+                TriggerEvent("player:savInvSv", _source)
                 break
+            else
+                TriggerEvent("item:new",_source, name, amount, identifier , charid)
             end
         end
-    
+    end
+
 end)
 
 AddEventHandler("item:new", function(source, item, quantity, identifier , charid)
-        local _source = source
-        for i,k in pairs(invTable) do
-            if k.id == identifier and k.charid == charid then
+    local _source = source
+    for i,k in pairs(invTable) do
+        if k.id == identifier and k.charid == charid then
 
-                local name = tostring(item)
-                local qty = tonumber(quantity)
-                k.inventory[(name)] = qty
-                TriggerClientEvent("gui:getItems", _source, k.inventory)
-                break
-            end
-        end
-   
+            local name = tostring(item)
+            local qty = tonumber(quantity)
+            k.inventory[(name)] = qty
+            TriggerClientEvent("gui:getItems", _source, k.inventory)
+            TriggerEvent("player:savInvSv", _source)
+            break end
+    end
+
 end)
 
 AddEventHandler("item:delete", function(source, arg, identifier , charid)
-        local _source = source
-        for i,k in pairs(invTable) do
-            if k.id == identifier and k.charid == charid then
-                local name = tostring(arg[1])
-                local qty = tonumber(arg[2])
-                local val = tonumber(k.inventory[name])
-                newVal = val - qty
-                k.inventory[name]= tonumber(newVal)
-                TriggerClientEvent("gui:getItems", _source, k.inventory)
-                break
-            end
-        end
+    local _source = source
+    for i,k in pairs(invTable) do
+        if k.id == identifier and k.charid == charid then
+            local name = tostring(arg[1])
+            local qty = tonumber(arg[2])
+            local val = tonumber(k.inventory[name])
+            newVal = val - qty
+            k.inventory[name]= tonumber(newVal)
+            TriggerClientEvent("gui:getItems", _source, k.inventory)
+            TriggerEvent("player:savInvSv", _source)
+            break end
+    end
 end)
 
 
@@ -146,11 +152,10 @@ AddEventHandler("item:onpickup", function(id)
             if k.id == identifier and k.charid == charid then
                 local pickup  = Pickups[id]
                 TriggerEvent("item:add", _source ,{pickup.name, pickup.amount}, identifier , charid)
-                TriggerClientEvent("item:Sharepickup", -1, pickup.name, pickup.obj , pickup.amount, x, y, z, 2) 
+                TriggerClientEvent("item:Sharepickup", -1, pickup.name, pickup.obj , pickup.amount, x, y, z, 2)
                 TriggerClientEvent('item:removePickup', -1, pickup.obj)
                 Pickups[id] = nil
                 TriggerClientEvent('gui:ReloadMenu', _source)
-                TriggerEvent("player:savInvSv", _source)
                 break
             end
         end
@@ -171,7 +176,6 @@ RegisterCommand('giveitem', function(source, args)
                 TriggerEvent("item:add", _source, {item, amount}, identifier , charid)
                 print("add")
                 TriggerClientEvent('gui:ReloadMenu', _source)
-                TriggerEvent("player:savInvSv", _source)
                 break
             end
         end
@@ -190,11 +194,10 @@ AddEventHandler("item:use", function(val)
         local charid = user.getSessionVar("charid")
         for i,k in pairs(invTable) do
             if k.id == identifier and k.charid == charid then
-                TriggerEvent("item:delete",_source, { name , amount}, identifier , charid)              
-                TriggerEvent("RegisterUsableItem:"..name)
+                TriggerEvent("item:delete",_source, { name , amount}, identifier , charid)
+                TriggerEvent("RegisterUsableItem:"..name, _source)
                 TriggerClientEvent("redemrp_notification:start", _source, "Item used: "..name, 3, "success")
                 TriggerClientEvent('gui:ReloadMenu', _source)
-                TriggerEvent("player:savInvSv", _source)
                 break
             end
         end
@@ -221,7 +224,6 @@ AddEventHandler("item:drop", function(val, amount)
                     TriggerClientEvent('item:pickup',_source, name, amount)
                     TriggerEvent("item:delete", _source, {name , amount}, identifier , charid)
                     TriggerClientEvent('gui:ReloadMenu', _source)
-                    TriggerEvent("player:savInvSv", _source)
                 end
                 break
             end
@@ -231,8 +233,8 @@ end)
 
 RegisterServerEvent("item:SharePickupServer")
 AddEventHandler("item:SharePickupServer", function(name, obj , amount, x, y, z)
-   TriggerClientEvent("item:Sharepickup", -1, name, obj , amount, x, y, z, 1) 
-   print("poszlo server")
+    TriggerClientEvent("item:Sharepickup", -1, name, obj , amount, x, y, z, 1)
+    print("poszlo server")
     Pickups[obj] = {
         name = name,
         obj = obj,
@@ -245,7 +247,7 @@ end)
 RegisterServerEvent("test_lols")
 AddEventHandler("test_lols", function(name, amount , target)
     local _target = target
-     local _source = source
+    local _source = source
     local _name = name
     local _amount = amount
     TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
@@ -255,14 +257,13 @@ AddEventHandler("test_lols", function(name, amount , target)
             if k.id == identifier and k.charid == charid then
                 local value = k.inventory[name]
                 local all = value-amount
-                   if all >= 0 then
-                       TriggerEvent("item:delete",_source, { name , amount}, identifier , charid)         
-                       TriggerEvent('test_lols222', _target , name , amount)
-                       TriggerClientEvent('gui:ReloadMenu', _source)
-                       TriggerEvent("player:savInvSv", _source)
-		       TriggerClientEvent("redemrp_notification:start", _source, "You have given: [X"..tonumber(amount).."]"..name.. " to " ..GetPlayerName(_target), 3, "success")
-		       TriggerClientEvent("redemrp_notification:start", _target, "You've received [X"..tonumber(amount).."]"..name.. " from " ..GetPlayerName(_source), 3, "success")
-                   end
+                if all >= 0 then
+                    TriggerEvent("item:delete",_source, { name , amount}, identifier , charid)
+                    TriggerEvent('test_lols222', _target , name , amount)
+                    TriggerClientEvent('gui:ReloadMenu', _source)
+                    TriggerClientEvent("redemrp_notification:start", _source, "You have given: [X"..tonumber(amount).."]"..name.. " to " ..GetPlayerName(_target), 3, "success")
+                    TriggerClientEvent("redemrp_notification:start", _target, "You've received [X"..tonumber(amount).."]"..name.. " from " ..GetPlayerName(_source), 3, "success")
+                end
                 break
             end
         end
@@ -279,22 +280,49 @@ AddEventHandler("test_lols222", function(source, name, amount)
         local charid = user.getSessionVar("charid")
         for i,k in pairs(invTable) do
             if k.id == identifier and k.charid == charid then
-		TriggerEvent("item:add",_source, {name, amount}, identifier , charid)        
-	        TriggerClientEvent('gui:ReloadMenu', _source)
-		TriggerEvent("player:savInvSv", _source)
-					
+                TriggerEvent("item:add",_source, {name, amount}, identifier , charid)
+                TriggerClientEvent('gui:ReloadMenu', _source)
+
                 break
             end
         end
     end)
 end)
 
+function inventory.checkItem(_source, name)
+    local value = 0
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+        local identifier = user.getIdentifier()
+        local charid = user.getSessionVar("charid")
+        for i,k in pairs(invTable) do
+            if k.id == identifier and k.charid == charid then
+                value = k.inventory[name]
+            end
+        end
+    end)
+    return tonumber(value)
+end
+function inventory.addItem(_source, name , amount)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+        local identifier = user.getIdentifier()
+        local charid = user.getSessionVar("charid")
+
+        TriggerEvent("item:add", _source ,{name, amount}, identifier , charid)
+    end)
+end
+
+function inventory.delItem(_source, name , amount)
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+        local identifier = user.getIdentifier()
+        local charid = user.getSessionVar("charid")
+
+        TriggerEvent("item:delete", _source, {name , amount}, identifier , charid)
+    end)
+end
 -----------------Register Usable item---------------
 RegisterServerEvent("RegisterUsableItem:wood")
 AddEventHandler("RegisterUsableItem:wood", function()
     print("test")
 end)
 ----------------------------------------------------
-
-
 
